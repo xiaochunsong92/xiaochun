1a8a83e8c7df324cf1d988f8202fc2e64a4a84f9
xiaochun <xiaochunsong92@163.com>
2018-10-10 00:07:02 +0800

    update

--- TianShuiSong/chinese_name_score/main.py
+++ TianShuiSong/chinese_name_score/main.py
@@ -81,13 +81,39 @@ def analyze():
     soup1 = BeautifulSoup(kangxi_result1, "html.parser")
     #print (soup.find(string=re.compile(u"汉字五行：")))
     kangxi_wx = soup.find(string=re.compile(u"汉字五行："))
+    kangxi_bihua = soup.find(string=re.compile(u"康熙笔画："))
     kangxi_wx = re.match(u".*?汉字五行：(.)", kangxi_wx)
+    kangxi_bihua = re.match(u".*?康熙笔画：(.)", kangxi_bihua)
     if kangxi_wx:
         kangxi_wx = kangxi_wx.group(1)
+    if kangxi_bihua:
+        kangxi_bihua = kangxi_bihua.group(1)
     kangxi_wx1 = soup1.find(string=re.compile(u"汉字五行："))
+    kangxi_bihua1 = soup1.find(string=re.compile(u"康熙笔画："))
     kangxi_wx1 = re.match(u".*?汉字五行：(.)", kangxi_wx1)
+    kangxi_bihua1 = re.match(u".*?康熙笔画：(.)", kangxi_bihua1)
     if kangxi_wx1:
         kangxi_wx1 = kangxi_wx1.group(1)
+    if kangxi_bihua1:
+        kangxi_bihua1 = kangxi_bihua1.group(1)
+    # Calculate shuliwuge 
+    tiange = int(kangxi_bihua) + 1
+    dige = int(kangxi_bihua1) + 1 
+    renge = int(kangxi_bihua) + int(kangxi_bihua1)
+    waige = 2
+    zongge = int(kangxi_bihua) + int(kangxi_bihua1)
+    shugemingli = open('wugeshuli', 'r')
+    shugemingli = shugemingli.readlines()
+    #for i in range(len(shugemingli)):
+    #    a = shugemingli[i]
+    #    tiange_jiedu = re.match(str(tiange) + '(\D.*)', a)
+    #    if tiange_jiedu:
+    #        tiange_jiedu = tiange_jiedu.group(1) 
+    #        print (tiange_jiedu)
+    #        print (shugemingli[i+1])
+    #        print (shugemingli[i+2])
+    #        print (shugemingli[i+3])
+    #        print (shugemingli[i+4])
     print('吕十大师五行八字分析')
     print('您的名字: %s %s' %(last_name, first_name))
     print('您的阳历生日: %s' %birthday) 
@@ -95,6 +121,63 @@ def analyze():
     print('您的生辰八字: %s %s %s %s %s %s %s %s' %(Gan[day.Lyear2.tg], Zhi[day.Lyear2.dz], Gan[day.Lmonth2.tg], Zhi[day.Lmonth2.dz], Gan[day.Lday2.tg], Zhi[day.Lday2.dz], Gan[gz.tg], Zhi[gz.dz]))
     print('您的生辰五行: 金 %s 木 %s 水 %s 火 %s 土 %s' %(WX['金'], WX['木'], WX['水'], WX['火'], WX['土']))
     print('您的名字五行: %s: %s %s: %s' %(last_name, kangxi_wx, first_name, kangxi_wx1))
+    print('您的数格命理: %s: %s %s: %s %s: %s %s: %s %s: %s' %('天格', tiange, '地格', dige, '人格', renge, '外格', waige, '总格', zongge))
+    print('您的数格命理分析:')
+    print('天格:')
+    for i in range(len(shugemingli)):
+        a = shugemingli[i]
+        tiange_jiedu = re.match(str(tiange) + '(\D.*)', a)
+        if tiange_jiedu:
+            tiange_jiedu = tiange_jiedu.group(1) 
+            print (tiange_jiedu)
+            print (shugemingli[i+1],end="")
+            print (shugemingli[i+2],end="")
+            print (shugemingli[i+3],end="")
+            print (shugemingli[i+4],end="")
+    print('地格:')
+    for i in range(len(shugemingli)):
+        a = shugemingli[i]
+        dige_jiedu = re.match(str(dige) + '(\D.*)', a)
+        if dige_jiedu:
+            dige_jiedu = dige_jiedu.group(1) 
+            print (dige_jiedu)
+            print (shugemingli[i+1],end="")
+            print (shugemingli[i+2],end="")
+            print (shugemingli[i+3],end="")
+            print (shugemingli[i+4],end="")
+    print('人格:')
+    for i in range(len(shugemingli)):
+        a = shugemingli[i]
+        renge_jiedu = re.match(str(renge) + '(\D.*)', a)
+        if renge_jiedu:
+            renge_jiedu = renge_jiedu.group(1) 
+            print (renge_jiedu)
+            print (shugemingli[i+1],end="")
+            print (shugemingli[i+2],end="")
+            print (shugemingli[i+3],end="")
+            print (shugemingli[i+4],end="")
+    print('外格:')
+    for i in range(len(shugemingli)):
+        a = shugemingli[i]
+        waige_jiedu = re.match(str(waige) + '(\D.*)', a)
+        if waige_jiedu:
+            waige_jiedu = waige_jiedu.group(1) 
+            print (waige_jiedu)
+            print (shugemingli[i+1],end="")
+            print (shugemingli[i+2],end="")
+            print (shugemingli[i+3],end="")
+            print (shugemingli[i+4],end="")
+    print('总格:')
+    for i in range(len(shugemingli)):
+        a = shugemingli[i]
+        zongge_jiedu = re.match(str(zongge) + '(\D.*)', a)
+        if zongge_jiedu:
+            zongge_jiedu = zongge_jiedu.group(1) 
+            print (zongge_jiedu)
+            print (shugemingli[i+1],end="")
+            print (shugemingli[i+2],end="")
+            print (shugemingli[i+3],end="")
+            print (shugemingli[i+4],end="")
 
 if __name__=='__main__':
     print('天水讼姓名测试系统')
